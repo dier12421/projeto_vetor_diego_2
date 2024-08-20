c49648efd2798fb0044eaba2e3a849d7ef8eef8c
dier <161588493+dier12421@users.noreply.github.com>
2024-08-20 08:48:08 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var meuFormulario = document.getElementById("formulario");
+meuFormulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  adicionarValor();
+  inverterVetor();
+  mostrarPares();
+});
+
+let vetor = [];
+
+function adicionarValor() {
+  const valor = Number(document.getElementById('valor').value);
+  vetor.push(valor);
+
+  const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+  const novalinha = tabela.insertRow();
+  const celula = novalinha.insertCell();
+  celula.textContent = valor;
+
+  document.getElementById('valor').value = '';
+}
+
+function inverterVetor() {
+  vetor.reverse(); // Inverte a ordem dos elementos do vetor
+}
+
+function mostrarPares() {
+  const tabela = document.getElementById("tabelaPares").getElementsByTagName("tbody")[0];
+
+  // Limpa a tabela antes de adicionar novos valores
+  tabela.innerHTML = '';
+
+  for (let i = 0; i < vetor.length; i++) {
+    const valor = vetor[i];
+    if (valor % 2 === 0) {
+      const linha = tabela.insertRow();
+      const celula = linha.insertCell();
+      celula.textContent = valor;
+    }
+  }
+}
